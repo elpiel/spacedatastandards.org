5287ffe398c71f6fa6cc76e6a80b12c161ab56b9
tj <tj@digitalarsenal.io>
2022-06-04 18:05:47 -0400

    deploy

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), viteCompression()],
   resolve: {
     alias: {
       'node-fetch': 'isomorphic-fetch',
